56f21395bd3db1858ff58445c37fd0ec233800c3
amaroelias <amaronascimento2016@hotmail.com>
2025-09-15 13:43:50 -0300

    iniciando projeto com spring gateway

---
+++ src/main/java/br/ufpb/dcx/apiGatewayConcessionaria/ApiGatewayConcessionariaApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.apiGatewayConcessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayConcessionariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayConcessionariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpb/dcx/apiGatewayConcessionaria/ApiGatewayConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.apiGatewayConcessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
